f67a392cdf25d252abf1beaff200ca4bbef2395a
Feesene <sene.felipe6@gmail.com>
2022-11-16 22:05:26 -0300

    modulo 5

--- Paradigmas de Ling. de Programação em Python/function.py
+++ Paradigmas de Ling. de Programação em Python/function.py
@@ -9,18 +9,19 @@ def func1():
     print(f'Função func1 - x = {x}')
 
 
-def func2():
+def func2(y):
     global x
-    x = 20
+    x = 20 + int(y)
     print(f'Função func2 - x = {x}')
+    return x
 
 
 x = 0
 func1()
-func2()
+func2(2)
 print(f'Programa principal - x = {x}')
 
 
 def regressiva(x):
-   print(x)
-   regressiva(x - 1)
+    print(x)
+    regressiva(x - 1)
